f89a0d26dc029441399c14a23f77e123dfb945df
Evgeniy Mineev <e.mineev@crazypanda.ru>
2019-12-03 18:13:54 +0400

    removed bad class for tests

--- Tests/Editor/CoroutineManagerFacadeTests.cs
+++ Tests/Editor/CoroutineManagerFacadeTests.cs
@@ -2,15 +2,37 @@ using System.Collections;
 using CrazyPanda.UnityCore.Flogs;
 using CrazyPanda.UnityCore.Network.HttpSystem;
 using NUnit.Framework;
+using UnityEngine.TestTools;
 
 namespace CrazyPanda.UnityCore.CoroutineSystem
 {
-    public sealed class CoroutineManagerFacadeSilentFailedTests
+    public sealed class CoroutineManagerFacadeTests
     {
+        private IFlogsManager _flogsManager;
         private ICoroutineManager _coroutineManager;
 
         [ SetUp ]
-        public void Initialize() => _coroutineManager = new CoroutineManagerFacade( new FlogsManagerV2( new FlogsConfig( "no", "test" ), new UnityHttpConnection( new HttpSettings() ) ) );
+        public void Initialize()
+        {
+            _flogsManager = new FlogsManagerV2( new FlogsConfig( "no", "test" ), new UnityHttpConnection( new HttpSettings() ) );
+            _coroutineManager = new CoroutineManagerFacade( _flogsManager );
+        }
+
+        [ UnityTest ]
+        public IEnumerator LogSendedToFlogsOnExceptionTest()
+        {
+            var logSended = false;
+            _flogsManager.OnSendException += ( sender, exception ) => logSended = true;
+
+            Assert.DoesNotThrow( () => _coroutineManager.StartCoroutine( null, null ) );
+
+            while( !logSended )
+            {
+                yield return null;
+            }
+
+            Assert.IsTrue( logSended );
+        }
 
         [ Test ]
         public void CreateProcessorFailedSilentOnNullValuesTest() => Assert.DoesNotThrow( () => _coroutineManager.CreateProcessor( null ) );
